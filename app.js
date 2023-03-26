a6aef0f141b1ed7756104b0f795df7a948413956
Raghav <raghav4u03@gmail.com>
2023-03-26 19:07:03 +0530

    Level 3 security

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 //jshint esversion:6
-require("dotenv").config();
+// require("dotenv").config();
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -20,10 +21,10 @@ async function main() {
     password: String,
   });
 
-  userSchema.plugin(encrypt, {
-    secret: process.env.SECRET,
-    encryptedFields: ["password"],
-  });
+  //   userSchema.plugin(encrypt, {
+  //     secret: process.env.SECRET,
+  //     encryptedFields: ["password"],
+  //   });
 
   const User = mongoose.model("User", userSchema);
 
@@ -38,7 +39,7 @@ async function main() {
   app.post("/login", async function (req, res) {
     const username = req.body.username;
     const foundUser = await User.findOne({ email: username });
-    if (foundUser.password === req.body.password) {
+    if (foundUser.password === md5(req.body.password)) {
       res.redirect("/secrets");
     } else {
       console.log("No such user");
@@ -53,7 +54,7 @@ async function main() {
   app.post("/register", function (req, res) {
     const newUser = new User({
       email: req.body.username,
-      password: req.body.password,
+      password: md5(req.body.password),
     });
     newUser.save();
     res.redirect("/secrets");
