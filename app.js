f059407a850757fccb96e4b29bbadd4fbf4321a8
Raghav <raghav4u03@gmail.com>
2023-03-27 13:31:21 +0530

    Final commit using passport

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const passport = require("passport");
+const passportLocal = require("passport-local");
+const passportLocalMongoose = require("passport-local-mongoose");
+const session = require("express-session");
+// const bcrypt = require("bcrypt");
+// const saltRounds = 10;
 // const encrypt = require("mongoose-encryption");
 // const md5 = require("md5");
 
@@ -15,13 +19,27 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.use(
+  session({
+    secret: "This is our little secret.",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/SecretDB");
 
   const userSchema = new mongoose.Schema({
     email: String,
     password: String,
+    secret: String,
   });
+
+  userSchema.plugin(passportLocalMongoose);
 
   //   userSchema.plugin(encrypt, {
   //     secret: process.env.SECRET,
@@ -29,6 +47,11 @@ async function main() {
   //   });
 
   const User = mongoose.model("User", userSchema);
+
+  passport.use(User.createStrategy());
+
+  passport.serializeUser(User.serializeUser());
+  passport.deserializeUser(User.deserializeUser());
 
   app.get("/", function (req, res) {
     res.render("home");
@@ -39,47 +62,74 @@ async function main() {
   });
 
   app.post("/login", async function (req, res) {
-    const username = req.body.username;
-    const foundUser = await User.findOne({ email: username });
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password,
+    });
 
-    if (foundUser) {
-      var result = await bcrypt.compare(req.body.password, foundUser.password);
-      if (result) {
-        res.redirect("/secrets");
-      } else res.redirect("/");
-    } else {
-      console.log("No such user");
-      res.redirect("/");
-    }
+    req.login(user, function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        passport.authenticate("local")(req, res, function () {
+          res.redirect("/secrets");
+        });
+      }
+    });
   });
 
   app.get("/register", function (req, res) {
     res.render("register");
   });
 
-  app.post("/register", async function (req, res) {
-    var hash = await bcrypt.hash(req.body.password, saltRounds);
-    const newUser = new User({
-      email: req.body.username,
-      password: hash,
-    });
-    newUser.save();
-    res.redirect("/secrets");
+  app.post("/register", function (req, res) {
+    User.register(
+      { username: req.body.username },
+      req.body.password,
+      function (err, user) {
+        if (err) {
+          console.log(err);
+          res.redirect("/register");
+        } else {
+          passport.authenticate("local")(req, res, function () {
+            res.redirect("/secrets");
+          });
+        }
+      }
+    );
   });
 
-  app.get("/secrets", function (req, res) {
-    res.render("secrets");
+  app.get("/secrets", async function (req, res) {
+    const foundUsers = await User.find({ secret: { $ne: null } });
+    if (foundUsers) res.render("secrets", { userWithSecrets: foundUsers });
   });
 
   app.get("/logout", function (req, res) {
-    res.render("home");
+    req.logout(function (err) {
+      if (err) {
+        console.log(err);
+      } else res.redirect("/");
+    });
   });
 
   app.get("/submit", function (req, res) {
-    res.render("submit");
+    if (req.isAuthenticated()) {
+      res.render("submit");
+    } else {
+      res.redirect("/login");
+    }
   });
 
-  app.post("/submit", function (req, res) {});
+  app.post("/submit", async function (req, res) {
+    const submittedSecet = req.body.secret;
+    // console.log(req.user);
+    const foundUser = await User.findById(req.user.id);
+    if (foundUser) {
+      foundUser.secret = req.body.secret;
+      foundUser.save();
+      res.redirect("/secrets");
+    }
+  });
 
   app.listen("3000", function () {
     console.log("Server started at port 3000");
